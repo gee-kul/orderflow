b1e8952d865908de5688a9bd3c9327215b12519e
gee-kul <305773647+gee-kul@users.noreply.github.com>
2026-07-16 18:12:39 +0300

    feat: implement order creation

---
+++ internal/order/errors.go
@@ -0,0 +1,15 @@
+package order
+
+import "errors"
+
+var (
+	ErrOrderIDRequired     = errors.New("order ID is required")
+	ErrCustomerIDRequired  = errors.New("customer ID is required")
+	ErrItemsRequired       = errors.New("order item is required")
+	ErrCurrencyRequired    = errors.New("currency is required")
+	ErrProductIDRequired   = errors.New("product ID is required")
+	ErrProductNameRequired = errors.New("product NAME is required")
+	ErrUnitPriceInvalid    = errors.New("unit price is invalid")
+	ErrQuantityInvalid     = errors.New("quantity is invalid")
+	ErrTotalAmountOverflow = errors.New("total amount overflow")
+)
